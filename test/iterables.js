2a6c52fab6cd8e9033342a54c619732fdd000d52
Zeke Sikelianos <zeke@sikelianos.com>
2019-03-01 17:45:57 -0600

    decaffeinate test files

---
+++ test/iterables.js
@@ -0,0 +1,24 @@
+const Liquid = require('..')
+
+describe('Iterable', function () {
+  describe('.cast', function () {
+    it("doesn't cast iterables", function () {
+      const iterable = new Liquid.Iterable()
+      return expect(Liquid.Iterable.cast(iterable)).to.equal(iterable)
+    })
+
+    return it('casts null/undefined to an empty iterable', () => expect(Liquid.Iterable.cast(null).toArray()).to.become([]))
+  })
+
+  describe('.slice', () =>
+    it('is abstract', () =>
+      expect(() => new Liquid.Iterable().slice()).to.throw(/not implemented/)
+    )
+  )
+
+  return describe('.last', () =>
+    it('is abstract', () =>
+      expect(() => new Liquid.Iterable().last()).to.throw(/not implemented/)
+    )
+  )
+})
